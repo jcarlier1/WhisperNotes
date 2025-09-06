5db9d83975f3763e98e965bd4026498091f55ff6
Juan Carlier <carlier1@msu.edu>
2025-09-06 10:08:06 +0200

    Fix device specification and update model compute type in transcribe_faster.py

--- transcribe_faster.py
+++ transcribe_faster.py
@@ -4,9 +4,9 @@ faster_whisper_model = 'nyrahealth/faster_CrisperWhisper'
 
 # Initialize the Whisper model
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 torch_dtype = "float16" if torch.cuda.is_available() else "float32"
-model = WhisperModel(faster_whisper_model, device=device, compute_type="float32")
+model = WhisperModel(faster_whisper_model, device=device, compute_type="float16")
 audio_path = "audios/Lecture 1 EvoPsy_converted_60_120.mp3"
 segments, info = model.transcribe(audio_path, beam_size=1, language='en', word_timestamps = False, without_timestamps= True)
 
